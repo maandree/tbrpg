4bac620447a326d3d71540223e0cc7f57fdd8b85
Mattias Andrée <maandree@operamail.com>
2012-12-13 00:22:11 +0100

    missed .hpp staging
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/Key.hpp
+++ src/Key.hpp
@@ -26,7 +26,7 @@
 #include <vector>
 #include <unordered_map>
 
-#include "QuestItem.hpp"
+#include "Item.hpp"
 
 
 
@@ -45,7 +45,7 @@ namespace tbrpg
   /**
    * Key item class
    */
-  class Key: public QuestItem
+  class Key: public Item
   {
   public:
     /**
--- src/QuestItem.hpp
+++ src/QuestItem.hpp
@@ -26,8 +26,6 @@
 #include <vector>
 #include <unordered_map>
 
-#include "Item.hpp"
-
 
 
 #include "hash.hpp"
@@ -45,7 +43,7 @@ namespace tbrpg
   /**
    * Quest item class
    */
-  class QuestItem: public Item
+  class QuestItem
   {
   public:
     /**
@@ -80,13 +78,6 @@ namespace tbrpg
      * @param  original  The object to clone
      */
     QuestItem(QuestItem&& original);
-    
-    /**
-     * Fork the object
-     * 
-     * @return  A fork of the object
-     */
-    virtual Object* fork() const;
     
     
     
