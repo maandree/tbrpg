6b8a31f61c82ebb9c001ee32c0e59961115f4597
Mattias Andrée <maandree@operamail.com>
2012-12-20 19:32:20 +0100

    add new class Calculator for character attribute calculations
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/Calculator.cc
@@ -0,0 +1,182 @@
+// -*- mode: c++, coding: utf-8 -*-
+/**
+ * tbrpg – Text based roll playing game
+ * 
+ * Copyright © 2012  Mattias Andrée (maandree@kth.se)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "Calculator.hpp"
+
+
+/**
+ * Text based roll playing game
+ * 
+ * DD2387 Program construction with C++
+ * Laboration 3
+ * 
+ * @author  Mattias Andrée <maandree@kth.se>
+ */
+namespace tbrpg
+{
+  /**
+   * Constructor
+   */
+  Calculator::Calculator() : Object()
+  {
+    this->class_inheritance.push_back(144);
+    ////TODO implement constructor
+  }
+  
+  /**
+   * Copy constructor
+   * 
+   * @param  original  The object to clone
+   */
+  Calculator::Calculator(const Calculator& original) : Object(original)
+  {
+    (void) original;
+
+  }
+  
+  /**
+   * Copy constructor
+   * 
+   * @param  original  The object to clone
+   */
+  Calculator::Calculator(Calculator& original) : Object(original)
+  {
+    (void) original;
+
+  }
+  
+  /**
+   * Move constructor
+   * 
+   * @param  original  The object to clone
+   */
+  Calculator::Calculator(Calculator&& original) : Object(original)
+  {
+    (void) original;
+
+  }
+  
+  /**
+   * Fork the object
+   * 
+   * @return  A fork of the object
+   */
+  Object* Calculator::fork() const
+  {
+    return (Object*)(new Calculator(*this));
+  }
+  
+  
+  
+  /**
+   * Destructor
+   */
+  Calculator::~Calculator()
+  {
+    ////TODO implement destructor
+  }
+  
+  
+  
+  /**
+   * Assignment operator
+   * 
+   * @param   original  The reference object
+   * @return            The invoked object
+   */
+  Calculator& Calculator::operator =(const Calculator& original)
+  {
+    (void) original;
+    Object::__copy__((Object&)*this, (Object&)original);
+    return *this;
+  }
+  
+  /**
+   * Assignment operator
+   * 
+   * @param   original  The reference object
+   * @return            The invoked object
+   */
+  Calculator& Calculator::operator =(Calculator& original)
+  {
+    (void) original;
+    Object::__copy__((Object&)*this, (Object&)original);
+    return *this;
+  }
+  
+  /**
+   * Move operator
+   * 
+   * @param   original  The moved object, its resourced will be moved
+   * @return            The invoked object
+   */
+  Calculator& Calculator::operator =(Calculator&& original)
+  {
+    (void) original;
+    std::swap((Object&)*this, (Object&)original);
+    return *this;
+  }
+  
+  
+  /**
+   * Equality evaluator
+   * 
+   * @param   other  The other comparand
+   * @return         Whether the instances are equal
+   */
+  bool Calculator::operator ==(const Calculator& other) const
+  {
+    return true;
+  }
+  
+  /**
+   * Inequality evaluator
+   * 
+   * @param   other  The other comparand
+   * @return         Whether the instances are not equal
+   */
+  bool Calculator::operator !=(const Calculator& other) const
+  {
+    return (*this == other) == false;
+  }
+  
+  /**
+   * Copy method
+   * 
+   * @param  self      The object to modify
+   * @param  original  The reference object
+   */
+  void Calculator::__copy__(Calculator& self, const Calculator& original)
+  {
+    self = original;
+  }
+  
+  /**
+   * Hash method
+   * 
+   * @return  The object's hash code
+   */
+  size_t Calculator::hash() const
+  {
+    size_t rc = 0;
+    return rc;
+  }
+  
+}
+
