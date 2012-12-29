9c75b8f77d049a2e109616f8621b438a5cba898a
Mattias Andrée <maandree@operamail.com>
2012-12-29 18:21:58 +0100

    that was already documented
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/GamePlay.cc
+++ src/GamePlay.cc
@@ -339,7 +339,7 @@ namespace tbrpg
     int multiplier = (roll >= this->game.rules.critical_hit) ? 2 : 1;
     if (multiplier == 2)
       std::cout << "Critical hit." << std::endl;
-    if ((player->stealth_on) && weapon.melee) /* TODO document that backstab multipler only applies to melée */
+    if ((player->stealth_on) && weapon.melee)
     {
       player->stealth_on = false;
       multiplier *= this->calc.getBackstabMultiplier(player->character);
