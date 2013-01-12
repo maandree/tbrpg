076f47b21a13ba5cd0136c1a640e2249cf928668
Mattias Andrée <maandree@operamail.com>
2013-01-12 21:11:33 +0100

    cp hpp to cc for InventoryExaminor with prototype to unimplemented implementation translation

---
+++ src/InventoryExaminor.cc
@@ -0,0 +1,56 @@
+// -*- mode: c++ , coding: utf-8 -*-
+/**
+ * tbrpg – Text based roll playing game
+ * 
+ * Copyright © 2012, 2013  Mattias Andrée (maandree@kth.se)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "InventorExaminor.hpp"
+
+
+/**
+ * Text based roll playing game
+ * 
+ * DD2387 Program construction with C++
+ * Laboration 3
+ * 
+ * @author  Mattias Andrée <maandree@kth.se>
+ */
+namespace tbrpg
+{
+  /**
+   * Construction
+   */
+  InventoryExaminor::InventoryExaminor();
+  
+  
+  
+  /**
+   * Destructor
+   */
+  InventoryExaminor::~InventoryExaminor();
+  
+  
+  
+  /**
+   * Examine a characters inventory
+   * 
+   * @param  character   The index of the active character
+   * @param  characters  The party members
+   */
+  void InventoryExaminor::examine(long character, GameCharacter* characters) const;
+  
+}
+
